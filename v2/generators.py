02e5b2f6799258352a1c0d7c7cfef8d51ec7f37b
NikolinaTomic <tomic.ninja@gmail.com>
2020-11-28 12:02:26 +0100

    generators2

--- v2/generators.py
+++ v2/generators.py
@@ -3,16 +3,6 @@ import math
 def generate_list(n):
     for i in range(1, n+1):
         yield i
-    
-
-def generate_prime_numbers_list(list):
-    prime_numbers_list = []
-    length = len(list)
-    for element in range(2,length+1):
-        if is_prime(element):
-            prime_numbers_list.append(element)
-    
-    return prime_numbers_list
 
 def is_prime(n):
     numbers = generate_list(n)
